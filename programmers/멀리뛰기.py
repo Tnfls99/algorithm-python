b78f75af98a1b11b219e7e103896db15b3cf7bf1
Surin Lee <leesurin99@gmail.com>
2022-10-01 01:09:36 +0900

    프로그래머스 12914 풀이

---
+++ programmers/멀리뛰기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    dp = [0] * (n+1)
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+        
+    dp[1] = 1
+    dp[2] = 2
+    
+    for t in range(3, n+1):
+        dp[t] = dp[t-1] + dp[t-2]
+    
+    return dp[n]%1234567
